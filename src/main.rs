cd8902f875805f13d711654dfb2eb22d0c2c9d98
Diego Della Vecchia <diego.dellavecchia3@gmail.com>
2025-12-26 21:33:22 +0100

    refactor: improve error handling

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,33 @@
 use std::env;
 use std::fs;
+use std::process;
 
 struct Config {
     query: String,
     file_path: String,
 }
 
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough args supplied");
+        };
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing input arguments: {err}");
+        process::exit(1);
+    });
 
     let file_content = fs::read_to_string(config.file_path).expect("File path needs to be valid");
 
     println!("With text : \n{file_content}");
-}
-
-fn parse_config(args: &[String]) -> Config {
-    let query = &args.get(1).expect("Please supply a query").clone();
-    let file_path = &args.get(2).expect("Please supply a file path").clone();
-    Config {
-        query: String::from(query),
-        file_path: String::from(file_path),
-    }
 }
