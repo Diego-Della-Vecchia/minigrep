4a030d22aaf417aa35981848d65df8ef85aaf6bf
Diego Della Vecchia <diego.dellavecchia3@gmail.com>
2025-12-26 19:41:18 +0100

    feat: basic program to open file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+
+    let query = &args.get(1).expect("Please supply a query");
+    let file_path = &args.get(2).expect("Please supply a file path");
+
+    let file_content = fs::read_to_string(file_path).expect("File path needs to be valid");
+
+    println!("With text : \n{file_content}");
 }
