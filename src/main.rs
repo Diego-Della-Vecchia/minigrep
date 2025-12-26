7d9c583414a814b8df6e2f092e04669bf63ad351
Diego Della Vecchia <diego.dellavecchia3@gmail.com>
2025-12-26 19:52:59 +0100

    refactor: config parser

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
 use std::env;
 use std::fs;
 
+struct Config {
+    query: String,
+    file_path: String,
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args.get(1).expect("Please supply a query");
-    let file_path = &args.get(2).expect("Please supply a file path");
+    let config = parse_config(&args);
 
-    let file_content = fs::read_to_string(file_path).expect("File path needs to be valid");
+    let file_content = fs::read_to_string(config.file_path).expect("File path needs to be valid");
 
     println!("With text : \n{file_content}");
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = &args.get(1).expect("Please supply a query").clone();
+    let file_path = &args.get(2).expect("Please supply a file path").clone();
+    Config {
+        query: String::from(query),
+        file_path: String::from(file_path),
+    }
 }
