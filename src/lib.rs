73bfcb515bcd583a42d2668decc39ac739a7805e
Diego Della Vecchia <diego.dellavecchia3@gmail.com>
2025-12-27 02:35:14 +0100

    refactor: search functions

--- src/lib.rs
+++ src/lib.rs
@@ -48,26 +48,19 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search_case_sensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
     let query_lower = query.to_lowercase();
 
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query_lower) {
-            results.push(line);
-        }
-    }
-    results
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query_lower))
+        .collect()
 }
 
 #[cfg(test)]
